7c89f074c2bc4c4a2ca8b5b973dbe811b83f575c
RobertNerd <robertcarlosmusembi@gmail.com>
2025-06-18 07:16:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import json
 st.title("Kenya Forest Service Image Table Extractor (Blue Numbers Only)")
 
 # Your OCR.Space API Key here (replace this with your actual key)
-OCR_SPACE_API_KEY = "K888957"
+OCR_SPACE_API_KEY = "3dP8D5BQdEr8Dzgfyr31BuNu118iFNGh2nq3PAWw6JEp"
 
 # Function to call OCR.Space API
 def ocr_space_image(file, overlay=False, api_key=OCR_SPACE_API_KEY, language='eng'):
